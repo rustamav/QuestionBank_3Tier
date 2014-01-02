97582e7e46f2d0235cb6008ab5cc8954348600ce
WanHrust <rustam.alashrafov@gmail.com>
2014-01-03 00:11:55 +0400

    Copied previous project

---
+++ QuestionBank_3Tier/src/Global.java
@@ -0,0 +1,9 @@
+/**
+ * Global holds global variables
+ * 
+ * @author Rustam Alashrafov, Abdykerim Erikov
+ * 
+ */
+public class Global {
+	static public int SCREEN_WIDTH = 1000, SCREEN_HEIGHT = 600;
+}
---
+++ QuestionBank_3Tier/src/ShareData.java
@@ -0,0 +1,13 @@
+/**
+ * ShareData class holds data that is shared between different classes.
+ * 
+ * @author Rustam Alashrafov, Abdykerim Erikov
+ * 
+ */
+public class ShareData {
+
+	static public String userFisrtName;
+	static public String userLastName;
+	static public int userTFScore = 0;
+	static public int userMCScore = 0;
+}
