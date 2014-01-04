2412c6b4318645979dd3df42783f9355fe8a1974
WanHrust <rustam.alashrafov@gmail.com>
2014-01-05 00:28:41 +0400

    delete all

--- QuestionBank_3Tier/src/Global.java
+++
@@ -1,9 +0,0 @@
-/**
- * Global holds global variables
- * 
- * @author Rustam Alashrafov, Abdykerim Erikov
- * 
- */
-public class Global {
-	static public int SCREEN_WIDTH = 1000, SCREEN_HEIGHT = 600;
-}
