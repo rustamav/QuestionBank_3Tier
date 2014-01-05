45c33a9ed88b76f34e21e90cf4d640baaf2a77ef
abdykerim <abdykerim.erikov@gmail.com>
2014-01-05 14:10:19 +0200

    working server

--- QuestionBank_3Tier/src/MultyThread.java
+++ QuestionBank_3Tier/src/MultyThread.java
@@ -10,9 +10,9 @@ public class MultyThread {
 		ServerSocket ss = new ServerSocket(2008);
 		System.out.println("Server Started");
 		while (true) {
-			new TinyHttpdConnection(ss.accept());
 			MultyThread.nClients++;
-			System.out.println("New connection started" + MultyThread.nClients);
+			new TinyHttpdConnection(ss.accept());
+			System.out.println("New connection started: " + MultyThread.nClients);
 		}
 	}
 } // sending the socket returned from accept to thread
@@ -21,15 +21,22 @@ class TinyHttpdConnection extends Thread {
 	Socket sock;
 	boolean isRunning;
 	int counter;
+	int TFScore;
+	int MCScore;
 	QuestionAnswerHolder h;
+	int name;
+	String user;
 	TinyHttpdConnection(Socket s) {
+		name = MultyThread.nClients;
 		sock = s;
-		counter=1;
+		counter = 1;
+		TFScore = 0;
+		MCScore = 0;
 		isRunning = true;
+		user="";
 		try {
 			h = new QuestionAnswerHolder();
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		setPriority(NORM_PRIORITY - 1);
@@ -41,75 +48,87 @@ class TinyHttpdConnection extends Thread {
 		Question q = null;
 		while (isRunning) {
 			try {
-//
-//				System.out.println("Client number " + MultyThread.nClients
-//						+ " connetcted");
 				OutputStream out = sock.getOutputStream();
 				PrintWriter pw = new PrintWriter(out, true);
 				InputStream clientIn = sock.getInputStream();
 				BufferedReader br = new BufferedReader(new InputStreamReader(
 						clientIn));
-				// System.out.println("Reachable");
 				String clientMessage = br.readLine().toUpperCase();
-				// System.out.println("Unreachable");
+				
 				if (clientMessage == null) {
 					sock.close();
-					System.out.println("Connection closed");
+					System.out.println("Connection " +  name + " closed because client is not responding");
 					isRunning = false;
 				} else
 					switch (clientMessage) {
+					case "REGUSER":
+						pw.println("Name");
+						clientMessage = br.readLine();
+						user = clientMessage;
+						break;
 					case "QUIT":
 						sock.close();
-						System.out.println("Connection closed");
+						System.out.println("Connection " + name + " closed because client quited");
 						isRunning = false;
 						break;
 					case "NEXTQUESTION":
-						if (counter<=20){
+						if (counter <= 20) {
 							q = h.getRandomQuestion(0);
 							pw.println(q.getQuestion());
-							}
-						else if (counter<=28){
+						} else if (counter == 21)
+							pw.println("STARTMC");
+						else if (counter <= 29) {
 							q = h.getRandomQuestion(1);
-							pw.println(q.getQuestion() + "#" + q.getA() + "#" + q.getB() + "#" + q.getC() + "#" + q.getD() + "#" + q.getE());
-							}
+							pw.println(q.getQuestion() + "#" + q.getA() + "#"
+									+ q.getB() + "#" + q.getC() + "#"
+									+ q.getD() + "#" + q.getE());
+						}
+						else if (counter == 30){
+							pw.println("STARTSCORE");
+							clientMessage = br.readLine();
+							if (clientMessage.equalsIgnoreCase("SENDSCORES"))
+								pw.println(TFScore + "#" + MCScore + "#" + user);
+						}
 						counter++;
 						// TODO
 						break;
 					case "TRUE":
 					case "FALSE":
-						if(clientMessage.equalsIgnoreCase(q.getCorrectAnswer()))
+						if (clientMessage
+								.equalsIgnoreCase(q.getCorrectAnswer())) {
 							pw.println("Correct");
-						else
+							TFScore += 3;
+						} else
 							pw.println("Wrong");
-						//TODO
 						break;
 					case "GETANSWER":
-						pw.println(q.getCorrectAnswer());
+						if (counter <= 21)
+							pw.println(q.getCorrectAnswer());
+						else if (counter <= 30 && counter>=22) {
+							pw.println(q.getCorrectAnswer() + "#"
+									+ q.getExplanation());
+							clientMessage = br.readLine();
+							if (clientMessage.equalsIgnoreCase(q.getCorrectAnswer()))
+								MCScore += 5;
+						}
 						break;
-					
 					}
-			}
-			catch(NullPointerException e){
+			} catch (NullPointerException e) {
 				isRunning = false;
-				System.out.println("Connection closed");
-			}
-			catch(SocketException e){
+				System.out.println("Connection " + name + " closed because client session ended.");
+			} catch (SocketException e) {
 				isRunning = false;
-				System.out.println("Connection2 closed");
+				System.out.println("Connection " + name + " closed.");
 				try {
 					sock.close();
 				} catch (IOException e1) {
-					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
-			}
-			catch (SQLException e) {
+			} catch (SQLException e) {
 				isRunning = false;
 				System.out.println("Database failure");
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			} finally {
 			}
